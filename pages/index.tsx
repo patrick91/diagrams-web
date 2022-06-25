48c13e9183b1b3fb5940efd4d07f0e12795cfeaf
Patrick Arminio <patrick.arminio@gmail.com>
2022-06-25 02:02:13 +0000

    Debounce

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,13 +3,14 @@ import Head from "next/head";
 import Script from "next/script";
 import CodeMirror from "@uiw/react-codemirror";
 import { python } from "@codemirror/lang-python";
-import React from "react";
+import React, { useMemo } from "react";
 import { resources } from "../lib/resources";
 
 // @ts-ignore
 import Viz from "viz.js";
 // @ts-ignore
 import { Module, render } from "viz.js/full.render";
+import debounce from "lodash.debounce";
 
 const PRE_CODE = `
 import diagrams
@@ -42,16 +43,40 @@ with Diagram("Grouped Workers", show=False, direction="TB"):
                   EC2("worker5")] >> RDS("events")
 `.trim();
 
+const Loading = () => (
+  <svg
+    className="animate-spin -ml-1 mr-3 h-5 w-5 text-blue-400 absolute top-2 right-2"
+    xmlns="http://www.w3.org/2000/svg"
+    fill="none"
+    viewBox="0 0 24 24"
+  >
+    <circle
+      className="opacity-25"
+      cx="12"
+      cy="12"
+      r="10"
+      stroke="currentColor"
+      strokeWidth="4"
+    ></circle>
+    <path
+      className="opacity-75"
+      fill="currentColor"
+      d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+    ></path>
+  </svg>
+);
+
 const renderDiagram = async (pythonCode: string) => {
   // @ts-ignore
   await window.pyodide.runPythonAsync(PRE_CODE + pythonCode);
 };
 
 function Editor() {
-  // todo: debounce this
-  const onChange = React.useCallback((value: string) => {
+  const render = (value: string) => {
     renderDiagram(value);
-  }, []);
+  };
+
+  const onChange = useMemo(() => debounce(render, 300), []);
 
   return (
     <CodeMirror
@@ -118,7 +143,11 @@ const Home: NextPage = () => {
           <Editor />
         </div>
 
-        <div id="chart"></div>
+        <div className="relative">
+          <Loading />
+
+          <div id="chart"></div>
+        </div>
       </div>
     </div>
   );
